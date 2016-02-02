5b348ce7d7bf6557305435ebfd7c4d3a17f33d35
oscarw3 <oscarw3@gmail.com>
2016-02-02 14:23:24 -0500

    started working on emails

---
+++ app/mailers/reservation_mailer.rb
@@ -0,0 +1,17 @@
+class ReservationMailer < ApplicationMailer
+	default from: "organizerish@gmail.com"
+
+	def reservation_start(reservation)
+    	@reservation = reservation
+    	@user = User.where(id: @reservation.occupied)
+    	mail(to: @user.email, subject: 'Your reservation has started!')
+  	end
+
+  	def resource_deleted(reservation)
+  	 	@reservation = reservation
+  	 	@user = User.where(id: @reservation.occupied)
+  	 	mail(to: @user.email, subject: 'The resource was deleted :(')
+  	end
+
+
+end
