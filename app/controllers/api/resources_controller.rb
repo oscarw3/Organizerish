547174ebdac9a8ae6f3ee922d682e3a3cf6d2427
oscarw3 <oscarw3@gmail.com>
2016-02-29 23:15:42 -0500

    added status 401 if not right permissions

--- app/controllers/api/resources_controller.rb
+++ app/controllers/api/resources_controller.rb
@@ -1,32 +1,46 @@
 class Api::ResourcesController < ApiController
 	respond_to :json
 	def index
+		
 		@resources = Resource.all
 		respond_with @resources
 	end
 
 	def create
-		@resource = Resource.new(resource_params)
- 		@resource.user_id = current_user.id
- 		@resource.save
-  		@resource.initialize_permissions
- 		respond_with @resource
+		if !current_user.has_resource_management?
+			render json: { error: 'Access Denied'}, status: 401
+		else
+			@resource = Resource.new(resource_params)
+	 		@resource.user_id = current_user.id
+	 		@resource.save
+	  		@resource.initialize_permissions
+
+	 		respond_with @resource
+	 	end
 
 	end
 
 	def update 
-		@resource = Resource.find(params[:id])
-		if @resource.update(resource_params)
-			respond_with @resource
+		if !current_user.has_resource_management?
+			render json: { error: 'Access Denied'}, status: 401
+		else
+			@resource = Resource.find(params[:id])
+			if @resource.update(resource_params)
+				respond_with @resource
+			end
 		end
 	end
 
 	def destroy
-		@resource = Resource.find(params[:id])
-		@resource.delete_reservations
-		@resource.delete_permissions
-		@resource.destroy
-		respond_with @resource
+		if !current_user.has_resource_management?
+			render json: { error: 'Access Denied'}, status: 401
+		else
+			@resource = Resource.find(params[:id])
+			@resource.delete_reservations
+			@resource.delete_permissions
+			@resource.destroy
+			respond_with @resource
+		end
 	end
 
 	private
